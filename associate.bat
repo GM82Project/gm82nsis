1f663e0234795dcd37762e590756239e6e0b40a2
omicronrex <65692652+omicronrex@users.noreply.github.com>
2025-04-24 22:48:20 -0300

    actually, it works. just run as admin

--- associate.bat
+++ associate.bat
@@ -1,10 +1,9 @@
 assoc .rv2=rv2file
-ftype rv2file="%CD%\gm82vp.exe" "%%1"
-
 assoc .gmk=gmkfile
 assoc .gm81=gm81file
 assoc .gm82=gm82file
 
+ftype rv2file="%CD%\gm82vp.exe" "%%1"
 ftype gmkfile="%CD%\GameMaker.exe" "%%1"
 ftype gm81file="%CD%\GameMaker.exe" "%%1"
 ftype gm82file="%CD%\GameMaker.exe" "%%1"
